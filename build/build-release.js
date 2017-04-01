5519fc6e2e17770efa3771bbf0ab39eb46496d66
唐亮 <tangliang@mofanghr.com>
2017-04-02 00:18:54 +0800

    'add-verify'

--- build/build-release.js
+++ build/build-release.js
@@ -1,7 +1,7 @@
 // https://github.com/shelljs/shelljs
 require('shelljs/global')
 const path = require('path')
-// require('../MP_verify_4gruzCH0ofsFW7xX.txt')
+// require('../MP_verify_SV9CeydnGwoViOYq.txt')
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.release.config')
 var config = require('./config').release
@@ -14,6 +14,7 @@ rm('-rf', config.assetsRoot)
 
 mkdir('-p', assetsPath)
 cp(path.resolve(__dirname,'../MP_verify_4gruzCH0ofsFW7xX.txt'), config.assetsRoot)
+cp(path.resolve(__dirname,'../MP_verify_SV9CeydnGwoViOYq.txt'), config.assetsRoot)
 
 webpack(webpackConfig, function (err, stats) {
   if (err) throw err
