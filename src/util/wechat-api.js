0f08889cc9858a54a17fe9d53934e617ae03612f
baotang <tlyspa@qq.com>
2017-04-05 13:15:30 +0800

    share

--- src/util/wechat-api.js
+++ src/util/wechat-api.js
@@ -4,7 +4,7 @@ let wx = window.wx;
 let config = (config)=>{
     return new Promise((resolve,reject)=>{
         wx.config({
-            debug: true,
+            debug: false,
             appId: config.appid,
             timestamp: parseInt(config.timestamp),
             nonceStr: config.noncestr,
@@ -97,9 +97,6 @@ let wechatPay = (obj)=>{
 
 let wechatShare = ({title, link = window.location.href, imgUrl, desc})=>{
     imgUrl = Util.postUrl(imgUrl);
-    alert(title)
-    alert(link)
-    alert(imgUrl)
     wx.onMenuShareTimeline({
         title, // 分享标题
         link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
